7fbabfeb782f17259e10731a0f799ccad49d8ed1
HypoCodeEmia <2198342394@qq.com>
2025-11-25 10:53:41 +0800

    feat(*): 项目初始化

---
+++ class_tool/src/main/java/com/tempsfteam/class_tool/ClassToolApplication.java
@@ -0,0 +1,13 @@
+package com.tempsfteam.class_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassToolApplication.class, args);
+    }
+
+}
---
+++ class_tool/src/test/java/com/tempsfteam/class_tool/ClassToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tempsfteam.class_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
