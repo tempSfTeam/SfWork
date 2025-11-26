d5c85177166174438bb9fb3ca6fce98237be266b
BUJIN-SWORD <3160794105@qq.com>
2025-11-26 17:53:04 +0800

    feat(config): MybatisPlus 分页

---
+++ class_tool/src/main/java/com/tempsfteam/class_tool/config/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.tempsfteam.class_tool.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : IMG
+ * @create : 2024/9/10
+ */
+@Configuration
+@MapperScan("com.tempsfteam.class_tool.mapper")
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
