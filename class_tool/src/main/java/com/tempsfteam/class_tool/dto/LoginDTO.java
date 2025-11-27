d838614a28d655741ea06061c6622dea4fff6e61
Hermionie41111 <3335961945@qq.com>
2025-11-27 10:06:51 +0800

    feat(dto): 补充登录用LoginDTO

---
+++ class_tool/src/main/java/com/tempsfteam/class_tool/dto/LoginDTO.java
@@ -0,0 +1,23 @@
+package com.tempsfteam.class_tool.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author ADACHI
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDTO {
+    private String account;
+    private String password;
+    private String uncheckedCode;
+
+    /**
+     * 验证码对应的唯一标识
+     */
+    private String uuid;
+
+}
