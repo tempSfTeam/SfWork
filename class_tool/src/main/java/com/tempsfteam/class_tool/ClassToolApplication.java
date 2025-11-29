cb931582f4c525803e499293f02881cbea0f472a
Hermionie41111 <3335961945@qq.com>
2025-11-29 12:04:30 +0800

    fix(): 修复日志时区问题

--- class_tool/src/main/java/com/tempsfteam/class_tool/ClassToolApplication.java
+++ class_tool/src/main/java/com/tempsfteam/class_tool/ClassToolApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableWebMvc
 public class ClassToolApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
         SpringApplication.run(ClassToolApplication.class, args);
     }
 
