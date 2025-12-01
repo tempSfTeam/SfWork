aeffdf7687d5cc451a7bfaac587d6c9c3ac7b56e
Hermionie41111 <3335961945@qq.com>
2025-12-01 12:58:21 +0800

    fix(): 更改默认头像

--- class_tool/src/main/java/com/tempsfteam/class_tool/constant/UserConst.java
+++ class_tool/src/main/java/com/tempsfteam/class_tool/constant/UserConst.java
@@ -4,7 +4,7 @@ package com.tempsfteam.class_tool.constant;
  * @author ADACHI
  */
 public class UserConst {
-    public final static String DEFAULT_AVATAR_PATH = "https://yunxclasses.cn:8089/cloudClassroom/api/file/image/courseIcon/ab45edf52420423db564ea4061db77fb.jpg";
+    public final static String DEFAULT_AVATAR_PATH = "";
     public final static Integer DEFAULT_SCHOOL_ID = 0;
     public final static Integer DEFAULT_CLASS_ID = 0;
 }
